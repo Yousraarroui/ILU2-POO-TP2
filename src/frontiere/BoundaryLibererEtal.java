471b98887a5fceddaf7f4a02c554e4e1fc5cea2e
yousra_arroui <youssara11@hotmail.fr>
2024-04-03 17:29:26 +0200

    Liberer Etal

--- src/frontiere/BoundaryLibererEtal.java
+++ src/frontiere/BoundaryLibererEtal.java
@@ -10,7 +10,22 @@ public class BoundaryLibererEtal {
 	}
 
 	public void libererEtal(String nomVendeur) {
-		//TODO a completer
+		boolean vendeurReconnu = controlLibererEtal.isVendeur(nomVendeur);
+		if (!vendeurReconnu) {
+			System.out.println("Mais vous n'etes pas inscrit sur notre marche aujourd'hui !");
+		}
+		else {
+			String[] donneesEtal = controlLibererEtal.libererEtal(nomVendeur);
+			boolean etalOccupe = Boolean.valueOf(donneesEtal[0]);
+			if (etalOccupe) {
+				String produit = donneesEtal[2];
+				int quantiteInitiale = Integer.valueOf(donneesEtal[3]);
+				int quantiteVendue = Integer.valueOf(donneesEtal[4]);
+				System.out.println("Vous avez vendu " + quantiteVendue + " sur " + quantiteInitiale + " " + produit + ".");
+				System.out.println("Au revoir " + nomVendeur + " , passez une bonne journée.");
+			}
+			
+		}
 	}
 
 }
